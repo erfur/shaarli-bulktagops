803664f983b8b67635d80b6f39bdd2d9c5827357
erfur <erfur@mailbox.org>
2022-04-21 07:49:13 +0300

    handle enter key, add/remove given tags

--- js/bulktagops.js
+++ js/bulktagops.js
@@ -16,6 +16,17 @@ const linkCheckboxes = document.querySelectorAll('.link-checkbox');
 
 });
 
+const generateToken = () => {
+    return Jws.sign({
+        header: {
+          alg: 'HS512',
+          typ: 'JWT' },
+        payload: { iat: Math.floor(Date.now() / 1000) },
+        secret: bar.getAttribute('key')
+      }
+    ).toString();
+}
+
 const bar = document.querySelector('.bulktaginput')
 const updateBar = () => {
   var tags;
@@ -27,19 +38,11 @@ const updateBar = () => {
   const linkCheckedCheckboxes = document.querySelectorAll('.link-checkbox:checked');
   // console.log("Checked links: " + [...linkCheckedCheckboxes].map((i) => i.value).toString());
   [...linkCheckedCheckboxes].forEach((link) => {
-    const token = Jws.sign({
-        header: {
-          alg: 'HS512',
-          typ: 'JWT' },
-        payload: { iat: Math.floor(Date.now() / 1000) },
-        secret: bar.getAttribute('key')
-      }
-    );
     // retreive each link's tags using REST API
     fetch(window.location.origin + '/api/v1/links/' + link.value.toString(),
       {
         headers: {
-          'Authorization': 'Bearer ' + token.toString(),
+          'Authorization': 'Bearer ' + generateToken(),
         }
       }
     )
@@ -70,9 +73,49 @@ updateBar();
 const searchTags = document.querySelector('.searchtags');
 bar.setAttribute('data-list', searchTags.getAttribute('data-list'));
 
+const setTags = () => {
+  const tags = bar.value.split(" ");
+  const linkCheckedCheckboxes = document.querySelectorAll('.link-checkbox:checked');
+  [...linkCheckedCheckboxes].forEach((link) => {
+    fetch(window.location.origin + '/api/v1/links/' + link.value.toString(),
+      {
+        headers: {
+          'Authorization': 'Bearer ' + generateToken(),
+        }
+      }
+    )
+    // TODO study async, see if this usage is correct
+    .then(async (response) => {
+      linkJson = await response.json(); //extract JSON from the http response
+      // console.log(linkJson);
+      [...tags].forEach((tag) => {
+        if (tag.charAt(0) == '-' && linkJson.tags.includes(tag.substring(1)))
+          linkJson.tags = linkJson.tags.filter((e) => e != tag.substring(1));
+        else if (!linkJson.tags.includes(tag))
+          linkJson.tags.push(tag);
+      });
+
+      fetch(window.location.origin + '/api/v1/links/' + link.value.toString(),
+        {
+          method: "PUT",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + generateToken(),
+          },
+          body: JSON.stringify(linkJson),
+        }
+      ).then(res => {
+        console.log("Request complete! response:", res);
+      });
+    });
+  });
+}
+
 bar.addEventListener('keydown', (k) => {
   // TODO handle enter and set entered/deleted tags
   if (event.key === "Enter") {
     event.preventDefault();
+    setTags();
+    alert("Done!")
   }
 })
